3a349360618bfb7735fd66f49e2eda37acaab880
rabieo <43918617+rabieo@users.noreply.github.com>
2022-12-14 13:49:55 -0500

    added api gateway

--- user-service/src/main/java/com/omarrabie/user/service/UserService.java
+++ user-service/src/main/java/com/omarrabie/user/service/UserService.java
@@ -32,7 +32,7 @@ public class UserService {
         Userz user = userRepository.findByUserId(userId);
 
         Department department =
-                restTemplate.getForObject("http://localhost:9001/departments/" + user.getDepartmentId()
+                restTemplate.getForObject("http://DEPARTMENT-SERVICE/departments/" + user.getDepartmentId()
                         ,Department.class);
 
         vo.setUser(user);
